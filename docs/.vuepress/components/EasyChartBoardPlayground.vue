e8dd107c9fe54ee11a5a861af623628237967a45
Hayoung Gam <hahhyoung@gmail.com>
2021-08-23 19:07:36 +0900

    Add: Set EasyChartBoard Component Props

--- docs/.vuepress/components/EasyChartBoardPlayground.vue
+++ docs/.vuepress/components/EasyChartBoardPlayground.vue
@@ -1,23 +1,219 @@
 <template>
   <div>
-    <select>
-      <option value="">환자 데이터</option>
-      <option value="">자동차 데이터</option>
-    </select>
-    <select>
-      <option value="">라이트 테마</option>
-      <option value="">다크 테마</option>
-    </select>
-    <button @click="color = 'red'">change to red</button>
     <easy-chart-board
-      :options="{ datasets: [], theme: 'dark', layoutInfo: {} }"
+      :series="[
+        {
+          id: 1,
+          data: [
+            {
+              name: '간판',
+              data: [27, 38, 19, 26, 19, 31, 27, 20, 28, 36, 37, 38],
+            },
+            {
+              name: '지인 소개',
+              data: [17, 11, 24, 26, 18, 14, 19, 12, 14, 17, 15, 16],
+            },
+            {
+              name: '소문',
+              data: [5, 5, 9, 12, 4, 6, 5, 3, 4, 4, 6, 8],
+            },
+            {
+              name: '홈페이지',
+              data: [2, 5, 4, 4, 2, 3, 6, 2, 2, 7, 2, 1],
+            },
+            {
+              name: '네이버 블로그',
+              data: [4, 1, 3, 1, 3, 3, 2, 2, 4, 4, 5, 4],
+            },
+            {
+              name: '카톡 채널',
+              data: [0, 0, 0, 1, 1, 0, 2, 1, 1, 0, 0, 0],
+            },
+            {
+              name: '기타',
+              data: [45, 1, 4, 7, 8, 7, 5, 3, 6, 4, 1, 1],
+            },
+          ],
+        },
+        {
+          id: 2,
+          data: [
+            186, 93, 30, 20, 21, 4, 20
+          ],
+        },
+        {
+          id: 3,
+          data: [
+            {
+              name: '총 진료비 (2019)',
+              type: 'column',
+              data: [46632870, 41584730, 52955270, 48567390, 58018330, 55539980, 58520700, 49065550, 50117730, 58495400, 48557530, 48517400],
+            },
+            {
+              name: '총 진료비 (2020)',
+              type: 'column',
+              data: [39924170, 41147250, 35482200, 40162390, 67997690, 64065550, 49789670, 46006220, 47063870, 50758110, 47677190, 49274240]
+            },
+            {
+              name: '성장률 (2019)',
+              type: 'line',
+              data: [null, null, null, null, null, null, null, null, 160, 34, 13, 32]
+            },
+            {
+              name: '성장률 (2020)',
+              type: 'line',
+              data: [-14, -1, -33, -17, 17, 15, -15, -6, -6, -13, -2, 2]
+            },
+          ],
+        },
+      ]"
+      :options="{
+        datasets: [
+          {
+            id: 1,
+            chart: {
+              type: 'bar',
+              height: 350,
+              stacked: true,
+              stackType: '100%',
+            },
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+            stroke: {
+              width: 1,
+              colors: ['#fff'],
+            },
+            title: {
+              text: '최근 유입 경로 추이',
+              align: 'center',
+              style: {
+                fontSize: '18px',
+                fontWeight: 'bold',
+              },
+            },
+            xaxis: {
+              categories: [
+                '2020-07',
+                '2020-08',
+                '2020-09',
+                '2020-10',
+                '2020-11',
+                '2020-12',
+                '2021-01',
+                '2021-02',
+                '2021-03',
+                '2021-04',
+                '2021-05',
+                '2021-06',
+              ],
+            },
+            fill: {
+              opacity: 1,
+            },
+            legend: {
+              position: 'bottom'
+            },
+          },
+          {
+            id: 2,
+            type: 'pie',
+            options: {
+              chart: {
+                type: 'pie',
+                height: 350,
+              },
+            },
+            title: {
+              text: '최근 유입 경로 (2021년)',
+              align: 'center',
+              style: {
+                fontSize: '18px',
+                fontWeight: 'bold',
+              },
+            },
+            labels: [
+              '간판',
+              '지인 소개',
+              '소문',
+              '홈페이지',
+              '네이버 블로그',
+              '카톡 채널',
+              '기타',
+            ],
+            fill: {
+              opacity: 1,
+            },
+            legend: {
+              position: 'bottom',
+            }
+          },
+          {
+            id: 3,
+            type: 'line',
+            xaxis: {
+              categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              labels: {
+                formatter: function(value) {
+                  return `${value}월`;
+                }
+              }
+            },
+            yaxis: [
+              {
+                seriesName: '총 진료비 (2019)',
+                axisTicks: {
+                  show: true
+                },
+                axisBorder: {
+                  show: true,
+                },
+                title: {
+                  text: '총 진료비'
+                },
+                min: 30000000
+              },
+              {
+                seriesName: '총 진료비 (2020)',
+                show: false
+              },
+              {
+                opposite: true,
+                seriesName: '성장률 (2019)',
+                axisTicks: {
+                  show: true
+                },
+                axisBorder: {
+                  show: true,
+                },
+                title: {
+                  text: '성장률'
+                }
+              },
+              {
+                seriesName: '성장률 (2020)',
+                show: false
+              },
+            ],
+            legend: {
+              position: 'bottom',
+            }
+          }
+        ]
+      }"
     />
   </div>
 </template>
 
 <script>
-export default {
+import EasyChartBoard from '../../../src/components/EasyChartBoard'
 
+export default {
+  components: {
+    EasyChartBoard
+  }
 }
 </script>
 
--- src/components/EasyChartBoard.vue
+++ src/components/EasyChartBoard.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>this is easy chart</div>
+  <div>
+    <apex-charts
+      v-for="(item, index) in options.datasets"
+      :key="item.id"
+      :type="item.type"
+      :series="series[index].data"
+      :options="item"
+    />
+  </div>
 </template>
 
 <script>
-export default {};
+import apexCharts from 'vue-apexcharts';
+
+export default {
+  components: {
+    apexCharts,
+  },
+  props: {
+    type: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    series: {
+      type: Array,
+      required: true,
+      default() {
+        return [];
+      },
+    },
+    options: {
+      type: Object,
+      required: true,
+      default() {
+        return {};
+      },
+    },
+  },
+  mounted() {
+    console.log(this.options);
+    console.log(this.series);
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
