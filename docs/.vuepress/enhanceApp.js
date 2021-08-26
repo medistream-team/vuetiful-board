f620ffae933a83f60bcac46472efaabb4673c739
Hayoung Gam <hahhyoung@gmail.com>
2021-08-26 15:17:22 +0900

    Modify: Change uuid import order

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 // 최종적으로는 ../../dist/easyChartBoard.min.js 가 되어야 합니다ㅎ
-import EasyChartBoard from '../../src/index.js';
 import Uuid from "vue-uuid";
+import EasyChartBoard from '../../src/index.js';
 
 export default ({ Vue }) => {
   Vue.use(Uuid);
