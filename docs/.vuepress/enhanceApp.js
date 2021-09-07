c0f0b5ad5ac9add5a0d3fd7fa94d13a9bd599331
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-09-07 20:52:13 +0900

    Modify: fix vuepress build error

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,7 @@
 import UUID from "vue-uuid";
 import '../../src/assets/scss/index.scss';
 
-export default ({ Vue, isServer }) => {
+export default async ({ Vue, isServer }) => {
   Vue.use(UUID);
   if(!isServer){
     await import('../../dist/vuetiful-board.umd.min.js').then(FullpageModalPlugin => {
